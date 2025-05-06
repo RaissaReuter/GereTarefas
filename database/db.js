b86775bfded8dcce9c5df35b45d82f43eb2f06a2
Raíssa Reuter <raissareuter@gmail.com>
2025-05-06 01:05:22 -0300

    Fazendo o db.js

---
+++ database/db.js
@@ -0,0 +1,14 @@
+// ARQUIVO: database/db.js
+const mongoose = require("mongoose");
+
+const connectToDb = () => {
+  mongoose
+    .connect(process.env.DB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
+    .then(() => console.log("MongoDB Atlas CONECTADO!"))
+    .catch((err) => console.log(err));
+};
+
+module.exports = connectToDb;
